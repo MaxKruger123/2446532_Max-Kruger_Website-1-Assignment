8c440b65c702668beefa18b2224f440ff954514d
MaxKruger123 <maxkruger321@gmail.com>
2023-05-27 17:14:06 +0200

    A tag to buttons on blogs

---
+++ JS/Blogs.js
@@ -0,0 +1,27 @@
+function week1Click(){
+    window.location.replace("Week1.html");
+}
+
+function week2Click(){
+    window.location.replace("Week2.html");
+}
+
+function week3Click(){
+    window.location.replace("Week3.html");
+}
+
+function week4Click(){
+    window.location.replace("Week4.html");
+}
+
+function week5Click(){
+    window.location.replace("Week5.html");
+}
+
+function week6Click(){
+    window.location.replace("Week6.html");
+}
+
+function week7Click(){
+    window.location.replace("Week7.html");
+}
