ed5f5011583c940a0da38ad2051721c749e6271d
MaxKruger123 <maxkruger321@gmail.com>
2023-06-02 12:48:29 +0200

    Fixed all Headers and JS

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,9 @@
 function getQuote(){
     let newQuote = document.getElementById("quote");
-
     let quotes = [];
-
-    quotes[0] = "\"There is a direct correlation between practice and result\"";
-    
-    quotes[1] = "\"The best teacher in life is pain\"";
-    
-
-    //Randomly generate quote
-
-    let i = Math.floor(Math.random() * quotes.length);
-
-    //set quote
-    newQuote.innerHTML = quotes[i];
-    
+    quotes[0] = "\"There is a direct correlation between practice and result\"";    
+    quotes[1] = "\"The best teacher in life is pain\"";    
+    let i = Math.floor(Math.random() * quotes.length);    
+    newQuote.innerHTML = quotes[i];    
 }
     
